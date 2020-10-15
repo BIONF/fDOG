13fec856d28185b4d10cdab54dffaca220da6ab4
trvinh <trvinh@gmail.com>
2020-10-15 09:48:56 +0200

    minor fix setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with greedyFAS.  If not, see <http://www.gnu.org/licenses/>.
+#  along with fdog.  If not, see <http://www.gnu.org/licenses/>.
 #
 #######################################################################
 
