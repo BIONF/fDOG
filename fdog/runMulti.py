4a213ab2e2337500bd389fabe50ff74b8e4f0f2a
trvinh <trvinh@gmail.com>
2021-01-13 13:50:46 +0100

    added parsing ete3 taxDB to setup

--- fdog/runMulti.py
+++ fdog/runMulti.py
@@ -180,7 +180,7 @@ def calcFAS (outpath, extendedFa, weightpath, cpu):
         sys.exit('Problem running\n%s' % (fasCmd))
 
 def main():
-    version = '0.0.13'
+    version = '0.0.14'
     parser = argparse.ArgumentParser(description='You are running fdogs.run version ' + str(version) + '.')
     parser.add_argument('--version', action='version', version=str(version))
     required = parser.add_argument_group('Required arguments')
