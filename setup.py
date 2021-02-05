6dab6705dc09301e15bf7bc7b103ed1b315d272a
trvinh <trvinh@gmail.com>
2021-02-05 17:16:52 +0100

    fixed pkg pyyaml

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md", "r") as input:
 
 setup(
     name="fdog",
-    version="0.0.19",
+    version="0.0.20",
     python_requires='>=3.7.0',
     description="Feature-aware Directed OrtholoG search tool",
     long_description=long_description,
@@ -41,7 +41,7 @@ setup(
         'tqdm',
         'ete3',
         'six',
-        'yaml',
+        'PyYAML',
         'greedyFAS>=1.5.0'
     ],
     entry_points={
