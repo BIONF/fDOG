014f0b686112bd63f33834bd263409b7b89b88cc
mueli94 <hannah.muelbaier@gmail.com>
2021-03-19 12:15:49 +0100

    testing

--- fdog/bin/oneSeq.pl
+++ fdog/bin/oneSeq.pl
@@ -381,7 +381,7 @@ GetOptions (
 	"scoringmatrix=s" => \$matrix
 );
 
-print " Evalue: $eval_blast \n"
+print " Evalue: $eval_blast \n";
 $outputPath = abs_path($outputPath);
 unless (-d $coreOrthologsPath) {
 	make_path($coreOrthologsPath);
