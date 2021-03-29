e8792774ae0825fb8cd28bae94f92caf1a46d5db
mueli94 <hannah.muelbaier@gmail.com>
2021-03-29 10:20:59 +0200

    testing

--- fdog/bin/oneSeq.pl
+++ fdog/bin/oneSeq.pl
@@ -790,7 +790,7 @@ if(!$coreOnly && !$assembly){
 if (assembly){
 	my $file_assembly_out;
 	$file_assembly_out = $outputPath . '/' . $seqName . '.extended.fa';
-	#print $file_assembly_out;
+	print $file_assembly_out;
 	my $cmd_merge;
 	$cmd_merge = "fdog.mergeAssembly --in  $outputPath --out  $file_assembly_out --cleanup";
 	print $cmd_merge;
