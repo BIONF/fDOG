3a3081e052e14fe0b019167573c22b85292cadc6
Hannah Muelbaier <47216555+mueli94@users.noreply.github.com>
2024-06-18 14:57:30 +0200

    Improvements fdog.addAssembly script (#47)
    * Updated fdog.addAssembly module
    
    * improved call of main fuction
    
    * Improved help function

--- fdog/addAssembly.py
+++ fdog/addAssembly.py
@@ -32,27 +32,26 @@ def parse_file(path):
         line = line.rstrip()
         ncbi, name = line.split("\t")
         id_dict[ncbi] = name
-
+    file.close()
     return id_dict
 
 
 def main():
-
+    print("#################################")
     #################### handle user input #####################################
-    version = '0.0.2'
+    version = '0.0.3'
     ################### initialize parser ######################################
     parser = argparse.ArgumentParser(description='You are running fdog.addAssembly version ' + str(version) + '.')
     ################## required arguments ######################################
     required = parser.add_argument_group('Required arguments')
     required.add_argument('--fasta', help='Path to fasta file or folder', action='store', default='', required=True)
     required.add_argument('--out', help='Path to output folder.', action='store', default='', required=True)
-    required.add_argument('--ncbi', help='NCBI number of species or mapping file', action='store', default='', required=True)
+    required.add_argument('--ncbi', help='NCBI ID of species or a mapping file (tab separated) containing the NCBI ID and the corresponding file name placed in the folder given by --fasta. ', action='store', default='', required=True)
     required.add_argument('--ver', help='Version', action='store', default='', required=True)
     optional = parser.add_argument_group('Optional arguments')
-    optional.add_argument('--link', help='link files not copy', action='store_true', default = True)
+    optional.add_argument('--link', help='links fasta files instead of copying them', action='store_true', default = False)
 
     args = parser.parse_args()
-
     fasta = args.fasta
     if check_path(fasta) == False:
         print("%s does not exists. Exiting ..."%(fasta))
@@ -68,21 +67,24 @@ def main():
     id_dict = {}
 
     if check_path(ncbi) == False:
-        if isdigit(ncbi) and format == "File":
-            id_dict[ncbi] = fasta_file
+        if ncbi.isdigit() and format == "File":
+            id_dict[ncbi] = fasta
         else:
             print("%s is no file or digit. Exiting ..."%(ncbi))
             sys.exit()
     elif check_path(ncbi) == "File":
+        print("Parsing mapping file ...")
         id_dict = parse_file(ncbi)
+        print("... done")
     else:
         print("%s is no file or digit. Exiting ..."%(ncbi))
         sys.exit()
-
+    #print(format)
+    #print(fasta)
     if format == "File":
-        fa = id_dict[id]
+        fa = id_dict[ncbi]
         if check_fasta(fa):
-            name = addTaxon_fn.generate_spec_name(id, "", ver)
+            name = addTaxon_fn.generate_spec_name(ncbi, "", ver)
             if ln == False:
                 assembly_folder = out_folder + name
                 os.system('mkdir %s >/dev/null 2>&1' % (assembly_folder))
@@ -93,23 +95,30 @@ def main():
                 os.system("ln %s %s/%s.fa" %(fa, assembly_folder, name))
         else:
             print("%s Fasta format not valid or header includes |"%(fa))
-
-    for id in id_dict:
-        fa = id_dict[id]
-        fasta = os.path.abspath(fasta) + '/'
-        if check_fasta(fasta + fa):
-            name = addTaxon_fn.generate_spec_name(id, "", ver)
-            if ln == False:
-                assembly_folder = out_folder + name
-                os.system('mkdir %s >/dev/null 2>&1' % (assembly_folder))
-                os.system("cp %s/%s %s/%s.fa" %(fasta, fa, assembly_folder, name))
+    
+    else:
+        for sp in id_dict:
+            print("Adding species %s"%(sp))
+            #print(id_dict)
+            fa = id_dict[sp]
+            fasta = os.path.abspath(fasta) + '/'
+            #print(fa)
+            #print(fasta)
+            fasta_path = fasta + fa
+            if check_fasta(fasta_path):
+                name = addTaxon_fn.generate_spec_name(sp, "", ver)
+                if ln == False:
+                    assembly_folder = out_folder + name
+                    os.system('mkdir %s >/dev/null 2>&1' % (assembly_folder))
+                    os.system("cp %s %s/%s.fa" %(fasta_path, assembly_folder, name))
+                else:
+                    assembly_folder = out_folder + name
+                    os.system('mkdir %s >/dev/null 2>&1' % (assembly_folder))
+                    os.system("ln -s %s %s/%s.fa" %(fasta_path, assembly_folder, name))
             else:
-                assembly_folder = out_folder + name
-                os.system('mkdir %s >/dev/null 2>&1' % (assembly_folder))
-                os.system("ln -s %s/%s %s/%s.fa" %(fasta, fa, assembly_folder, name))
-        else:
-            print("%s Fasta format not valid or header includes |"%(fa))
+                print("%s Fasta format not valid or header includes |"%(fasta_path))
 
     print("DONE, files can be found: %s"%(out_folder))
 
-main()
+if __name__ == '__main__':
+    main()
