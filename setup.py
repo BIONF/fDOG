c150b471ff725a3bc4054059b4efeedc8e782410
AnnamTran <trvinh@gmail.com>
2025-04-25 13:33:15 +0200

    version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md", "r") as input:
 
 setup(
     name="fdog",
-    version="0.1.40",
+    version="1.0.0",
     python_requires='>=3.12.0',
     description="Feature-aware Directed OrtholoG search tool",
     long_description=long_description,
