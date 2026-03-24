057e9cf4c26d1f0675608e9f02ffe6a9cf6394c5
trvinh <trvinh@gmail.com>
2026-03-24 12:10:04 +0100

    rm blastdb from pre-calculated data

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md", "r") as input:
 
 setup(
     name="fdog",
-    version="1.1.5",
+    version="1.1.6",
     python_requires='>=3.12.0',
     description="Feature-aware Directed OrtholoG search tool",
     long_description=long_description,
